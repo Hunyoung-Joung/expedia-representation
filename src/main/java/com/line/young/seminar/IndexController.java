70fd25e76ddd518c89e44132a5902952692bbdea
Hunyoung Joung <jounghunyoung@gmail.co>
2019-06-19 13:32:17 +0900

    fix application file

--- src/main/java/com/line/young/seminar/IndexController.java
+++ src/main/java/com/line/young/seminar/IndexController.java
@@ -1,15 +1,30 @@
 package com.line.young.seminar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.SQLException;
 import java.util.logging.Logger;
+
+import javax.sql.DataSource;
 
 @Controller
 public class IndexController {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
+    
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -49,5 +64,16 @@ public class IndexController {
         logger.info("------------------ survey");
 
         return "survey";
+    }
+    
+    @Bean
+    public DataSource dataSource() throws SQLException {
+      if (dbUrl == null || dbUrl.isEmpty()) {
+        return new HikariDataSource();
+      } else {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+      }
     }
 }
