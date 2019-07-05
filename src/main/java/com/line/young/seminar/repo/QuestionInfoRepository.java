84075f31a5f0d935504fad995d22cb99efa87f5a
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-05 12:20:32 +0900

    test to insert

--- src/main/java/com/line/young/seminar/repo/QuestionInfoRepository.java
+++ src/main/java/com/line/young/seminar/repo/QuestionInfoRepository.java
@@ -2,19 +2,22 @@ package com.line.young.seminar.repo;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import com.line.young.seminar.entity.QuestionInfo;
 
 public interface QuestionInfoRepository extends CrudRepository<QuestionInfo, String> {
     
 //    @PersistenceContext
-//    private EntityManager entityManager;
+//    private final EntityManager entityManager;
 //
-//    public List<Account> findByname(String name){
-//      return entityManager.createNamedQuery("Account.findByusername", Account.class)
-//        .setParameter("name", name)
+//    @SuppressWarnings("unchecked")
+//    public default List<QuestionInfo> findAllUserQuestion(String userId){
+//      return entityManager.createNamedQuery("QuestionInfo.findAllUserQuestion")
+//        .setParameter("userId", userId)
 //        .getResultList();
 //    }
 //    List<QuestionInfo> findAllUserQuestion(String userId);
--- src/main/java/com/line/young/seminar/service/QuestionInfoService.java
+++ src/main/java/com/line/young/seminar/service/QuestionInfoService.java
@@ -3,6 +3,9 @@ package com.line.young.seminar.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.line.young.seminar.entity.QuestionInfo;
@@ -13,7 +16,7 @@ public class QuestionInfoService {
     
     @Autowired
     private QuestionInfoRepository questionInfoRepository;
-    
+
 
     public Iterable<QuestionInfo> findAllOfQuestionInfo() {
         return questionInfoRepository.findAll();
