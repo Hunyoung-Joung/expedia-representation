e65ddf20aae4954fdff791869c560a4fa517071c
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-08 18:21:58 +0900

    test to insert

--- src/main/java/com/line/young/seminar/service/SurveyInfoService.java
+++ src/main/java/com/line/young/seminar/service/SurveyInfoService.java
@@ -17,38 +17,38 @@ import com.line.young.seminar.repo.QuestionInfoRepository;
 public class SurveyInfoService {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
     
-    @Autowired
-    private QuestionInfoRepository questionInfoRepository;
-    
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @SuppressWarnings("unchecked")
-    public List<QuestionInfo> findByUserId(String userId){
-      return entityManager.createNamedQuery("QuestionInfo.findAllUserQuestion")
-        .setParameter("userId", userId)
-        .getResultList();
-    }
-
-
-    public Iterable<QuestionInfo> findAllOfQuestionInfo() {
-        return questionInfoRepository.findAll();
-    }
-    
-    public List<QuestionInfo> findAllUserQuestion(String userId) {
-        logger.info("## QuestionInfoService findAllUserQuestion userId? "+userId);
-        return this.findByUserId(userId);
-    }
-
-    public QuestionInfo saveOfQuestionInfo(QuestionInfo questionInfo) {
-        return questionInfoRepository.save(questionInfo);
-    }
-
-    public void deleteOfQuestionInfo(String userId) {
-        questionInfoRepository.deleteById(userId);
-    }
-    
-    public void deleteAllOfQuestionInfo() {
-        questionInfoRepository.deleteAll();
-    }
+//    @Autowired
+//    private QuestionInfoRepository questionInfoRepository;
+//    
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @SuppressWarnings("unchecked")
+//    public List<QuestionInfo> findByUserId(String userId){
+//      return entityManager.createNamedQuery("QuestionInfo.findAllUserQuestion")
+//        .setParameter("userId", userId)
+//        .getResultList();
+//    }
+//
+//
+//    public Iterable<QuestionInfo> findAllOfQuestionInfo() {
+//        return questionInfoRepository.findAll();
+//    }
+//    
+//    public List<QuestionInfo> findAllUserQuestion(String userId) {
+//        logger.info("## QuestionInfoService findAllUserQuestion userId? "+userId);
+//        return this.findByUserId(userId);
+//    }
+//
+//    public QuestionInfo saveOfQuestionInfo(QuestionInfo questionInfo) {
+//        return questionInfoRepository.save(questionInfo);
+//    }
+//
+//    public void deleteOfQuestionInfo(String userId) {
+//        questionInfoRepository.deleteById(userId);
+//    }
+//    
+//    public void deleteAllOfQuestionInfo() {
+//        questionInfoRepository.deleteAll();
+//    }
 }
