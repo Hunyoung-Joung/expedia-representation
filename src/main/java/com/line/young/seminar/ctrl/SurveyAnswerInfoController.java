c3bc9b2cac674079809a08f94da2d388fcc743e7
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-10 14:44:51 +0900

    test to insert

--- src/main/java/com/line/young/seminar/ctrl/SurveyAnswerInfoController.java
+++ src/main/java/com/line/young/seminar/ctrl/SurveyAnswerInfoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -138,7 +139,7 @@ class SurveyAnswerInfoRestController {
     private SurveyAnswerInfoController surveyAnswerInfoController = new SurveyAnswerInfoController();
 //    @RequestMapping(value={"/survey/api/add"})
     @PostMapping("/survey/api/add")
-    public String addtquestionInfo(Model model, @ModelAttribute String userId, @ModelAttribute String[] surveyAnswerInfos) 
+    public String addtquestionInfo(Model model, @Valid @RequestBody String userId, @Valid @RequestBody String[] surveyAnswerInfos) 
             throws Exception  {
         logger.info("##### add surveyAnswerInfo information");
         
