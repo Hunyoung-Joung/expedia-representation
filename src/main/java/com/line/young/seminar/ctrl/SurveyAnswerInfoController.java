011f29a3a7611311e0d215565f9853ff86c0930e
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-11 13:50:09 +0900

    test to insert

--- src/main/java/com/line/young/seminar/ctrl/SurveyAnswerInfoController.java
+++ src/main/java/com/line/young/seminar/ctrl/SurveyAnswerInfoController.java
@@ -149,10 +149,12 @@ class SurveyAnswerInfoRestController {
         
         JsonParser jsonParser = new BasicJsonParser();
         Map<String, Object> jsonMap = null;
+        Map<String, Object> jsonMap_ = null;
 //
         jsonMap = jsonParser.parseMap(surveyAnswerInfoList);
         String userId = jsonMap.get("userId").toString();
         String list = jsonMap.get("surveyAnswerInfos").toString();
+        jsonParser.parseList(list);
 //        
 //        List<Object> list = springParser.parseList(jsonMap.get("surveyAnswerInfos").toString());
 
