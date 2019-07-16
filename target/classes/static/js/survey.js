757a18f338f113a6dd8f114c4a66c1ea9779c201
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-16 16:59:40 +0900

    test to insert

--- target/classes/static/js/survey.js
+++ target/classes/static/js/survey.js
@@ -14,13 +14,6 @@ var checkBoxVals = [];
 
 $(document).ready(function(){
 
-//	JSON.parse(initData).each(function() {
-//		var target_id = $(this).attr("id");
-//		var target_number = target_id.substring(1,2);
-//		var obj = {"seminar_id":seminarId,"user_id":userId,"survey_no":target_number,"survey_answer": $(this).val()};
-//		surveyInfo.push(obj);
-//	});
-
 	// when range bar value changes, then the answer field  is changed follow to the range bar value
 	// some of browser couldn't detect class selector
 	$('input[type=range]').on('input', function () {
