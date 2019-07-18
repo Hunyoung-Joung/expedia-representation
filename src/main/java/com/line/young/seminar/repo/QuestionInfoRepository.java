bb19f8bc904fa660dcb4e37d216ad013c9c4839d
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-18 16:01:40 +0900

    test to insert

--- src/main/java/com/line/young/seminar/repo/QuestionInfoRepository.java
+++ src/main/java/com/line/young/seminar/repo/QuestionInfoRepository.java
@@ -17,5 +17,5 @@ import com.line.young.seminar.entity.QuestionInfo;
  */
 public interface QuestionInfoRepository extends CrudRepository<QuestionInfo, String> {
 
-    List<QuestionInfo> findAllrQuestionByUserId(@Param("encryptId") String encryptId); // need to @named query name SurveyAnswerInfo.findAnswerByUserId"
+    List<QuestionInfo> findAllrQuestionByEncryptId(@Param("encryptId") String encryptId); // need to @named query name SurveyAnswerInfo.findAllrQuestionByEncryptId"
 }
