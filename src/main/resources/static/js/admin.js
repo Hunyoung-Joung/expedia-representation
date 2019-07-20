7e591e783024160645868873ab168a2d4e8cd11d
unknown <jounghunyoung@gmail.com>
2019-07-20 15:43:54 +0900

    from home

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -55,7 +55,22 @@ $(document).ready(function(){
 	});
     
     // Array stack to reverse order by question no
-    $.each(questionList, function(idx, val) {
+    $.each(questionList.reverse(), function(idx, val) {
+    	var addHtml = $('.q tbody').html('
+          		<tr style="word-wrap: break-word">
+        		<td class="col-sm-3"></td>
+        		<td></td>
+        		<td class="col-sm-3"></td>
+        		<td class="col-sm-5"></td>
+        		<td class="col-sm-2">
+          			<form th:action="@{/seminar/{q_no}(q_no=*{q_no})}" th:method="update">
+            			<input class="btn btn-default btn-xs" type="submit" value="削除" />
+          			</form>
+        		</td>
+        		<td class="col-sm-2"></td>
+      		</tr>'
+    	);
+    	$('.q tbody').append(addHtml);
         $('.q').each(function() {
         	$(this).find('tbody').each(function() {
         		$(this).find('tr').each(function() {
@@ -66,7 +81,7 @@ $(document).ready(function(){
         		});
         	});        	
     	});
-        $('.q tbody').append($('.q tbody').html());
+
 	});
 });
 
