3d601c29431cb8bca1457298e4e1f863a3a06cde
unknown <jounghunyoung@gmail.com>
2019-07-22 01:34:40 +0900

    from home

--- target/classes/static/js/admin.js
+++ target/classes/static/js/admin.js
@@ -73,12 +73,13 @@ $(document).ready(function(){
 			"<td class='col-sm-3'></td>" +
 			"<td class='col-sm-5'></td>" +
 			"<td class='col-sm-2'>" +
-//			"<form th:action='@{/seminar/{q_no}(q_no=*{q_no})}' th:method='put' th:object='${questionInfo}'>" +
+			"<form action='' method='post' >" +
+			"<input type='hidden' name='_method' value='put'>" +
 			"<div class='radio text-center'>" +
 			"<label><input id='isSelected1' name='is_selected' type='radio' value='true' >可能</label> " +
 			"<label><input id='isSelected2' name='is_selected' type='radio' value='false' >不可</label>" +
 			"</div>" +
-//			"</form>" +
+			"</form>" +
 			"</td>" +
 			"<td class='col-sm-2'></td>" +
 			"</tr>";
