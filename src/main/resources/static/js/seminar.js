bb88673ff6b6074014294f239fde074b856e9626
unknown <jounghunyoung@gmail.com>
2019-07-21 21:38:55 +0900

    from home

--- src/main/resources/static/js/seminar.js
+++ src/main/resources/static/js/seminar.js
@@ -3,33 +3,19 @@
  * by jounghunyoung@gmail.com
  * 
  */
-// API key
-// API key
-var apiKey = "";
-// elements
-var userId = "";
-var seminarId = "";
+
 var category = "";
 var content = "";
 
 $(document).ready(function(){
 
 	$("#show_map").click(function(){
-        window.open("https://www.relo-kaigi.jp/comfort/shinjyuku/access/",'popUpWindow','height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+        window.open('https://www.relo-kaigi.jp/comfort/shinjyuku/access/','popUpWindow','height=500,width=500,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=yes');
 	});
 	
-	$("textarea#content").mouseover(function(){
-		$(this).val("");
-	}).mouseleave(function(){
-		if ($(this).val() == "") {
-			$(this).val("セミナー内容に対して質問をしてください");
-		}
-	});
-	
-	// send question
-	$("button#submit").click(function(){
-		category = $($('select#category').find(":selected")).val();
-		content = $("textarea#content").val();
+	$("#submit").click(function(){
+		category = $($('#qCategory').find(":selected")).val();
+		content = $("#qContents").val();
 		
 		if ((content == "") || (content == "セミナー内容に対して質問をしてください") || (category == "0")) {
 			showError("エラー：カテゴリーが選択されていないか、質問が入力されていません");
