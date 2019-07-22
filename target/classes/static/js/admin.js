1575197319e5d91121ade91f97cc96147f7cabfc
Hunyoung Joung <jounghunyoung@gmail.co>
2019-07-22 11:03:14 +0900

    test to insert

--- target/classes/static/js/admin.js
+++ target/classes/static/js/admin.js
@@ -95,7 +95,8 @@ $(document).ready(function(){
             				if (i == 1) {
             					$($(this).find("#qNo")).val(val[i]);
             				} else if (i == 4) {
-//            					console.log(" -- "+$(this).find("form").attr());
+            					console.log(" -- "+$(this).find("form"));
+            					$(this).find("form").attr("action","/seminar/"+val[1]);
 //            					$($(this).find("form")).prop('"action=/seminar/"+val[1]');
             					if (val[i] == true) {
             						$($(this).find("#isSelected1")).prop("checked", true);
