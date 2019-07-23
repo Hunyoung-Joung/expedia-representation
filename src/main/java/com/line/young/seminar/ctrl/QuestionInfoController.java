2f99ef2eec1ac046fa83f27f268bf05a8b9fe7ad
unknown <jounghunyoung@gmail.com>
2019-07-24 02:38:12 +0900

    from home

--- src/main/java/com/line/young/seminar/ctrl/QuestionInfoController.java
+++ src/main/java/com/line/young/seminar/ctrl/QuestionInfoController.java
@@ -1,40 +1,34 @@
+/**
+ * Copyright@ LINE 
+ * by jounghunyoung@gmail.com
+ * 
+ */
 package com.line.young.seminar.ctrl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.line.young.seminar.entity.PersonalInfo;
 import com.line.young.seminar.entity.QuestionInfo;
-import com.line.young.seminar.entity.SurveyAnswerInfo;
 import com.line.young.seminar.service.PersonalInfoService;
 import com.line.young.seminar.service.QuestionInfoService;
-import com.line.young.seminar.service.SurveyAnswerInfoService;
-
-
 
 /**
  * 
- * @author JP22601
+ * Index controller
+ * 
+ * @author jounghunyoung@gmail.com
  *
  */
 @Controller
@@ -51,6 +45,13 @@ public class QuestionInfoController {
     
     private String encryptId_ = "";
     
+    /**
+     * Get index
+     * 
+     * @param encryptId
+     * @param model
+     * @return
+     */
     @GetMapping
     public String init(@RequestParam("encryptId") String encryptId, Model model) {
         this.encryptId_ = encryptId;
@@ -80,7 +81,13 @@ public class QuestionInfoController {
         }
     }
 
-
+    /**
+     * Find by user id
+     * 
+     * @param encryptId
+     * @param questionInfos
+     * @return
+     */
     @RequestMapping(value="{encryptId}", method=RequestMethod.GET)
     public List<QuestionInfo> findByEncryptId(@PathVariable String encryptId, @ModelAttribute List<QuestionInfo> questionInfos) {
         if (!questionInfoService.findByEncryptId(encryptId).isEmpty()) {
@@ -91,15 +98,30 @@ public class QuestionInfoController {
         return questionInfos;
     }
     
+    /**
+     * Post save
+     * 
+     * @param model
+     * @param questionInfo
+     * @return
+     */
     @PostMapping
     public String addtquestionInfo(Model model, @ModelAttribute QuestionInfo questionInfo) {
         questionInfo.setEncrypt_id(this.encryptId_);
         questionInfo.setSeminar_id("3"); // TODO
         questionInfo = questionInfoService.saveOfQuestionInfo(questionInfo);
-//        return init(questionInfo.getEncrypt_id(), model);
-        return "redirect:/seminar?encryptId="+this.encryptId_;
+        return "redirect:/seminar?encryptId="+this.encryptId_; // PRG pattern
     }
     
+    /**
+     * Delete by question no
+     * 
+     * @param model
+     * @param q_no
+     * @param questionInfo
+     * @return
+     * @throws Exception
+     */
     @DeleteMapping("{q_no}")
     public String deleteById(Model model, @PathVariable Long q_no, @ModelAttribute QuestionInfo questionInfo) throws Exception {
     	questionInfoService.deleteById(q_no);
