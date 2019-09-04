9f67a0c385bc72cd188f6e930eec3bb0e411f6de
unknown <jounghunyoung@gmail.com>
2019-09-05 01:27:52 +0900

    make it better

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
 	
     $("#person_count_modal_confirm").click(function(){
     	$("#child_ages_field").each(function () {
-    		console.log(idx+" - "+$(val).);
+    		console.log(idx+" - "+$(val).attr());
     		$(this).find("input").each(function(idx, val){
     			console.log(idx+" - "+$(val).attr("id"));
     		});
