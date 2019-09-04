34f687fce9e3af29b78574246797ae9cf24b7c1c
unknown <jounghunyoung@gmail.com>
2019-09-04 15:47:10 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -9,7 +9,7 @@ var questionList = [];
 
 $(document).ready(function(){
 	
-    $("#input-number-group").on('click', function () {
+    $("#input-number-group").on('mousedown', function () {
     	console.log($(this).children().attr("id"));
     	var adult_count = $("#adult_count").val()+1;
     	 $("#adult_count").val(adult_count);
