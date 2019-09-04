e5554147f674f6a7b8ff875b985d60f29f0d81d9
unknown <jounghunyoung@gmail.com>
2019-09-04 16:38:28 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -8,16 +8,31 @@ var adminQuestionInfos;
 var questionList = [];
 
 $(document).ready(function(){
-	
+	// occupancy=2-9,4 -> adult 2, 9 years old 1, 4 years old 1
     $(".input-number-group").on('mousedown', function () {
-    	console.log($(this).children().attr("id"));
+    	var person_type = $(this).children().attr("id")
     	$(this).children().each(function(idx, val) {
+    		if (person_type == "adult_count_increase") {
+    			if (idx == 0) {
+    				$(val).val(parseInt($(val).val())+1)
+    			} 
+    			if (idx == 2) {
+    				$(val).val(parseInt($(val).val())-1)
+    			} 
+    		} else {
+    			if (idx == 0) {
+    				$(val).val(parseInt($(val).val())+1)
+    			} 
+    			if (idx == 2) {
+    				$(val).val(parseInt($(val).val())-1)
+    			} 
+    		}
 
 			console.log(idx+" - "+val);
 
 		});
-    	var adult_count = $("#adult_count").val()+1;
-    	 $("#adult_count").val(adult_count);
+//    	var adult_count = $("#adult_count").val()+1;
+//    	 $("#adult_count").val(adult_count);
 	});
 	
 });
