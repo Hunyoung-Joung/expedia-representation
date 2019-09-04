81cfb647964dffed29b5c4f3d926c780daf0896f
unknown <jounghunyoung@gmail.com>
2019-09-05 00:53:36 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 			if (child_count < 6) {
 				child_count = parseInt($("#child_count").val())+1;
 				$("#child_count").val(child_count);
-				$("#child_ages_field").append("<div class='form-group w-25'><input type='text' class='form-control' id='child_ages_field_"+child_count+"' readonly></div>");
+				$("#child_ages_field").append("<div class='col-xs-1'><input type='text' class='form-control' id='child_ages_field_"+child_count+"' readonly></div>");
 				$("#child_ages").append("<li id='child_age_"+child_count+"' class='find_out'><div class='dropdown'>" +
 						"<button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>子供年齢 "+child_count+"<span class='caret'></span></button>" +
 						"<ul class='dropdown-menu'>" +
@@ -80,7 +80,7 @@ $(document).ready(function(){
     		if ($(this).attr("class") == "find_out") {
     			return $(this).attr("id");
     		}
-    	}).get();
+    	}).get().split("child_age_")[1];
 
     	console.log("id? "+id);
     	var val = $(event.target).text();
