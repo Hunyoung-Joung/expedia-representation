3a91c5dfc0dee8457fbfcbe5bf7fbe680be90091
unknown <jounghunyoung@gmail.com>
2019-09-04 16:19:44 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
 	
     $(".input-number-group").on('mousedown', function () {
     	console.log($(this).children().attr("id"));
-    	$(this).each(function() {
-			var target_id = $(this).attr("id");
-			console.log(target_id);
+    	$(this).children().each(function(idx, val) {
+
+			console.log(idx+" - "+val);
 
 		});
     	var adult_count = $("#adult_count").val()+1;
