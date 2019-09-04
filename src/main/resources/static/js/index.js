1c91f3dd28671d4b19e02a61732ee5b063cf06fb
unknown <jounghunyoung@gmail.com>
2019-09-04 21:33:57 +0900

    make it better

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -32,24 +32,24 @@ $(document).ready(function(){
 				$("#child_ages").append("<div class='dropdown'>" +
 						"<button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>　年齢 "+child_count+"　<span class='caret'></span></button" +
 						"<ul class='dropdown-menu'>" +
-						"<li><a href="#">0</a></li>" +
-						"<li><a href="#">1</a></li>" +
-						"<li><a href="#">2</a></li>" +
-						"<li><a href="#">3</a></li>" +
-						"<li><a href="#">4</a></li>" +
-						"<li><a href="#">5</a></li>" +
-						"<li><a href="#">6</a></li>" +
-						"<li><a href="#">7</a></li>" +
-						"<li><a href="#">8</a></li>" +
-						"<li><a href="#">9</a></li>" +
-						"<li><a href="#">10</a></li>" +
-						"<li><a href="#">11</a></li>" +
-						"<li><a href="#">12</a></li>" +
-						"<li><a href="#">13</a></li>" +
-						"<li><a href="#">14</a></li>" +
-						"<li><a href="#">15</a></li>" +
-						"<li><a href="#">16</a></li>" +
-						"<li><a href="#">17</a></li>" +
+						"<li><a href='#'>0</a></li>" +
+						"<li><a href='#'>1</a></li>" +
+						"<li><a href='#'>2</a></li>" +
+						"<li><a href='#'>3</a></li>" +
+						"<li><a href='#'>4</a></li>" +
+						"<li><a href='#'>5</a></li>" +
+						"<li><a href='#'>6</a></li>" +
+						"<li><a href='#'>7</a></li>" +
+						"<li><a href='#'>8</a></li>" +
+						"<li><a href='#'>9</a></li>" +
+						"<li><a href='#'>10</a></li>" +
+						"<li><a href='#'>11</a></li>" +
+						"<li><a href='#'>12</a></li>" +
+						"<li><a href='#'>13</a></li>" +
+						"<li><a href='#'>14</a></li>" +
+						"<li><a href='#'>15</a></li>" +
+						"<li><a href='#'>16</a></li>" +
+						"<li><a href='#'>17</a></li>" +
 						"</ul></div>");
 			}
 		} else if (id == "child_count_decrease") {
