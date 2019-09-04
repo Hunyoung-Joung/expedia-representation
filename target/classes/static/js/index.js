ffe3bd96b6e66bd41bbe1002f58d1e1236eb3fa3
unknown <jounghunyoung@gmail.com>
2019-09-04 17:55:38 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -15,39 +15,37 @@ $(document).ready(function(){
     		
     		var adult_count = parseInt($("#adult_count").val());
     		var child_count = parseInt($("#child_count").val());
+    		var id = $(val).attr("id");
     		
-    		var id = $(val).attr("id")
     		if (id == "adult_count_increase") {
     			adult_count = parseInt($("#adult_count").val())+1;
     			$("#adult_count").val(adult_count);
     			console.log(adult_count+", "+$("#adult_count").val());
     			return false;
-    		} 
-    		else if (id == "adult_count_decrease") {
+    		} else if (id == "adult_count_decrease") {
     			if (adult_count > 0) {
     				adult_count = parseInt($("#adult_count").val())-1;
     				$("#adult_count").val(adult_count);
     				console.log(adult_count+", "+$("#adult_count").val());
     			}
-    			return;
-    		} 
-    		else if (id == "child_count_increase") {
+    			return false;
+    		} else if (id == "child_count_increase") {
     			child_count = parseInt($("#child_count").val())+1;
     			$("#child_count").val(child_count);
     			console.log(adult_count+", "+$("#child_count").val());
-    			return;
-    		} 
-    		else if (id == "child_count_decrease") {
+    			return false;
+    		} else if (id == "child_count_decrease") {
     			if (child_count > 0) {
     				child_count = parseInt($("#child_count").val())-1;
     				$("#child_count").val(child_count);
     				console.log(adult_count+", "+$("#child_count").val());
     			}
-    			return;
+    			return false;
     		} 
 		});
-//    	var adult_count = $("#adult_count").val()+1;
-//    	 $("#adult_count").val(adult_count);
 	});
 	
+    $("#person_count_modal_confirm").click(function(){
+    	$("#person_count").val("大人"+$("#adult_count").val()++","+"子供"+$("#child_count").val());
+	});
 });
