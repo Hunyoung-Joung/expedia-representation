cf9e675d651c44ead5aab199885bc495423be3c2
unknown <jounghunyoung@gmail.com>
2019-09-04 17:15:52 +0900

    make it better

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -13,28 +13,30 @@ $(document).ready(function(){
     	var person_type = $(this).children().attr("id")
     	$(this).children().each(function(idx, val) {
     		console.log($(val).attr("id"));
-    	
-//    		if (person_type == "adult_count_increase") {
-//    			var adult_count = parseInt($("#adult_count").val());
-//    			console.log(adult_count);
-//    			if (idx == 0) {
-//    				adult_count =+ 1;
-//    			} 
-//    			if (idx == 2) {
-//    				if (adult_count != 0) {
-//    					adult_count =+ 1;
-//    				}
-//    			} 
-//    			$("#adult_count").val(adult_count);
-//    		} else {
-//    			if (idx == 0) {
-//
-//    			} 
-//    			if (idx == 2) {
-//
-//    			} 
-//    		}
-//
+    		var adult_count = parseInt($("#adult_count").val());
+    		var child_count = parseInt($("#child_count").val());
+    		var id = $(val).attr("id")
+    		if (id == "adult_count_increase") {
+    			adult_count += 1;
+    			$("#adult_count").val(adult_count);
+    		} 
+    		if (id == "adult_count_decrease") {
+    			if (adult_count != 0) {
+    				adult_count -= 1;
+    				$("#adult_count").val(adult_count);
+    			}
+    		} 
+    		if (id == "child_count_increase") {
+    			child_count += 1;
+    			$("#child_count").val(adult_count);
+    		} 
+    		if (id == "child_count_decrease") {
+    			if (child_count != 0) {
+    				child_count -= 1;
+    				$("#child_count").val(adult_count);
+    			}
+    		} 
+
 		});
 //    	var adult_count = $("#adult_count").val()+1;
 //    	 $("#adult_count").val(adult_count);
