65db571412b5bb3141eda86dc88a9e81cd45c303
unknown <jounghunyoung@gmail.com>
2019-09-04 16:17:07 +0900

    make it better

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -11,6 +11,11 @@ $(document).ready(function(){
 	
     $(".input-number-group").on('mousedown', function () {
     	console.log($(this).children().attr("id"));
+    	$(this).each(function() {
+			var target_id = $(this).attr("id");
+			console.log(target_id);
+
+		});
     	var adult_count = $("#adult_count").val()+1;
     	 $("#adult_count").val(adult_count);
 	});
