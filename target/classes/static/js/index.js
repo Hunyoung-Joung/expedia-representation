62c1d6890dfed9d6cfb5c65898be8cf5bddfe918
unknown <jounghunyoung@gmail.com>
2019-09-04 19:06:00 +0900

    make it better

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -10,42 +10,35 @@ var questionList = [];
 $(document).ready(function(){
 	// occupancy=2-9,4 -> adult 2, 9 years old 1, 4 years old 1
     $(".input-number-group").on('mousedown', function (event) {
+    	var id = event.target.id;
+		var adult_count = parseInt($("#adult_count").val());
+		var child_count = parseInt($("#child_count").val());
+		
+		if (id == "adult_count_increase") {
+			adult_count = parseInt($("#adult_count").val())+1;
+			$("#adult_count").val(adult_count);
+			console.log(adult_count+", "+$("#adult_count").val());
 
-//    	$(this).children().each(function(idx, val) {
-    		console.log("event.target? "+event.target.nodeName);
-    		console.log("event.target? "+event.target.id);
-    		console.log("event.target? "+$(this).target);
-    		var adult_count = parseInt($("#adult_count").val());
-    		var child_count = parseInt($("#child_count").val());
-//    		var id = $(val).attr("id");
-    		var id = $(event.target).attr("id");
-    		
-    		if (id == "adult_count_increase") {
-    			adult_count = parseInt($("#adult_count").val())+1;
-    			$("#adult_count").val(adult_count);
-    			console.log(adult_count+", "+$("#adult_count").val());
+		} else if (id == "adult_count_decrease") {
+			if (adult_count > 0) {
+				adult_count = parseInt($("#adult_count").val())-1;
+				$("#adult_count").val(adult_count);
+				console.log(adult_count+", "+$("#adult_count").val());
+			}
 
-    		} else if (id == "adult_count_decrease") {
-    			if (adult_count > 0) {
-    				adult_count = parseInt($("#adult_count").val())-1;
-    				$("#adult_count").val(adult_count);
-    				console.log(adult_count+", "+$("#adult_count").val());
-    			}
+		} else if (id == "child_count_increase") {
+			child_count = parseInt($("#child_count").val())+1;
+			$("#child_count").val(child_count);
+			console.log(adult_count+", "+$("#child_count").val());
 
-    		} else if (id == "child_count_increase") {
-    			child_count = parseInt($("#child_count").val())+1;
-    			$("#child_count").val(child_count);
-    			console.log(adult_count+", "+$("#child_count").val());
+		} else if (id == "child_count_decrease") {
+			if (child_count > 0) {
+				child_count = parseInt($("#child_count").val())-1;
+				$("#child_count").val(child_count);
+				console.log(adult_count+", "+$("#child_count").val());
+			}
 
-    		} else if (id == "child_count_decrease") {
-    			if (child_count > 0) {
-    				child_count = parseInt($("#child_count").val())-1;
-    				$("#child_count").val(child_count);
-    				console.log(adult_count+", "+$("#child_count").val());
-    			}
-
-    		} 
-//		});
+		} 
 	});
 	
     $("#person_count_modal_confirm").click(function(){
