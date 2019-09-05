3482a4bfc85424f13f56c62b327ece61085f5c0e
unknown <jounghunyoung@gmail.com>
2019-09-06 02:22:46 +0900

    make it better

--- src/main/java/com/expedia/young/demo/ctrl/indexController.java
+++ src/main/java/com/expedia/young/demo/ctrl/indexController.java
@@ -1,25 +1,16 @@
 /**
- * Copyright@ LINE 
+ * Copyright@
  * by jounghunyoung@gmail.com
  * 
  */
 package com.expedia.young.demo.ctrl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.logging.Logger;
-import java.util.zip.GZIPInputStream;
-
 import javax.validation.Valid;
-
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -28,50 +19,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-
-import com.expedia.young.demo.entity.AdminQuestionInfo;
 import com.expedia.young.demo.entity.ConditionInfo;
-import com.expedia.young.demo.entity.PersonalInfo;
 import com.expedia.young.demo.entity.Properties;
 import com.expedia.young.demo.entity.PropertiesAvailability;
-import com.expedia.young.demo.entity.QuestionInfo;
 import com.expedia.young.demo.entity.Region;
-import com.expedia.young.demo.entity.UserInfo;
 import com.expedia.young.demo.entity.keyInfo;
-import com.expedia.young.demo.repo.QuestionInfoRepository;
-import com.expedia.young.demo.repo.UsersRepository;
-import com.expedia.young.demo.service.PersonalInfoService;
-import com.expedia.young.demo.service.QuestionInfoService;
-import com.expedia.young.demo.service.SurveyAnswerInfoService;
-import com.expedia.young.demo.service.SurveyInfoService;
 import com.expedia.young.demo.util.AuthHeaderValueSingleton;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -94,9 +61,6 @@ public class indexController {
     
     @GetMapping
     public String init(Model model) {
-    	
-//        model.addAttribute("conditionInfo", new ConditionInfo());
-
         return "index";
     }
     
@@ -258,21 +222,4 @@ public class indexController {
     	return PropertiesList;
     }
     
-//    /**
-//     * Update user question will answer or not.
-//     * 
-//     * @param model
-//     * @param q_no
-//     * @param questionInfo
-//     * @return
-//     */
-//    @PutMapping("{q_no}")
-//    public String update(Model model, @PathVariable Long q_no, @ModelAttribute QuestionInfo questionInfo) {
-//        boolean is_selected = questionInfo.isIs_selected();
-//        questionInfo = questionInfoService.findById(q_no).get();
-//        questionInfo.setIs_selected(is_selected);
-//        questionInfoService.saveOfQuestionInfo(questionInfo);
-//        model.addAttribute("userInfo", userInfo_);
-//        return index(model, userInfo_);
-//    }
 }
