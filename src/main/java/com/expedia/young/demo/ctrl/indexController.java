6586015343372153bc4ebaddb7d48b2522c85184
unknown <jounghunyoung@gmail.com>
2019-09-05 09:43:41 +0900

    make it better

--- src/main/java/com/expedia/young/demo/ctrl/indexController.java
+++ src/main/java/com/expedia/young/demo/ctrl/indexController.java
@@ -122,23 +122,23 @@ public class indexController {
     	HttpEntity<?> entity = new HttpEntity<>(headers);
     	String url = keyInfo.getUri()+"regions/"+conditionInfo.getRegion_id()+"?region_id="+conditionInfo.getRegion_id()+"&language=en-US&include=details&include=property_ids";
     
-    	ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-    	JsonParser springParser = JsonParserFactory.getJsonParser();
-    	List<Object> list = springParser.parseList(response.toString());
-    	for(Object o : list) {
-    		if(o instanceof Map) {
-    			Map<String,Object> map = (Map<String,Object>) o;
-    			System.out.println("Items found: " + map.size());
-
-    			int i = 0;
-    			for (Map.Entry<String, Object> entry : map.entrySet()) {
-    				System.out.println(entry.getKey() + " = " + entry.getValue());
-    				i++;
-    			}
-
-    		}
-    	}
-    	
+//    	ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+//    	JsonParser springParser = JsonParserFactory.getJsonParser();
+//    	List<Object> list = springParser.parseList(response.toString());
+//    	for(Object o : list) {
+//    		if(o instanceof Map) {
+//    			Map<String,Object> map = (Map<String,Object>) o;
+//    			System.out.println("Items found: " + map.size());
+//
+//    			int i = 0;
+//    			for (Map.Entry<String, Object> entry : map.entrySet()) {
+//    				System.out.println(entry.getKey() + " = " + entry.getValue());
+//    				i++;
+//    			}
+//
+//    		}
+//    	}
+//    	
     	logger.info("######################response? "+response.getBody());
 
     	return "index";
